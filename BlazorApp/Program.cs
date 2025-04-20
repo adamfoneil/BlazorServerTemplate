8bdcaf3fd1432b2d4a8024149abb72351a064dee
Adam O'Neil <adamosoftware@gmail.com>
2025-04-19 21:33:03 -0400

    stuff

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -4,13 +4,13 @@ using BlazorApp.Extensions;
 using CoreNotify.MailerSend.Extensions;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Radzen;
 using Service.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddRadzenComponents();
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
